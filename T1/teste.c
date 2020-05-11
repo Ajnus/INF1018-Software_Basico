5fabee6f4d1d8dd8890eeeb81ae0100c109c52bd
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-05-11 13:03:55 -0300

    preparando para escrita em novos arquivos

--- T1/teste.c
+++ T1/teste.c
@@ -15,12 +15,16 @@ int main(void)
 		if (utf8==NULL) {fputs ("erro ao abrir arquivo",stderr); exit (1);}
 	FILE* utf32 = fopen("utf32_peq.txt", "rb");
 		if (utf32==NULL) {fputs ("erro ao abrir arquivo",stderr); exit (1);}
+	FILE* utf8out = fopen("utf8_peq_out.txt", "wb");
+		if (utf8out==NULL) {fputs ("erro ao abrir arquivo",stderr); exit (1);}
+	FILE* utf32out = fopen("utf32_peq_out.txt", "wb");
+		if (utf32out==NULL) {fputs ("erro ao abrir arquivo",stderr); exit (1);}
 	
 
 	printf("O processador deste computador e: %s Endian.\n", isLittleEndian() ? "Little" : "Big");
 	
-	utf8_32(utf8, utf32);
-	utf32_8(utf32, utf8);
+	utf8_32(utf8, utf32out);
+	utf32_8(utf32, utf8out);
 	
 	fclose(utf8);
 	fclose(utf32);
