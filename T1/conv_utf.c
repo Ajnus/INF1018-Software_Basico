8a314cc6a46372e2857b4fc464d6cc655c69e5e1
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-05-13 14:01:58 -0300

    utf32_8: LER: OK AVALIAR BOM OK: CONVERTER: OK IMPRIMIR: NA TELA OK, ARQUIVO NÃO

--- T1/conv_utf.c
+++ T1/conv_utf.c
@@ -220,22 +220,269 @@ int utf32_8(FILE* arq_entrada, FILE* arq_saida)
 		dump (&bufferWrite[0], wSize);*/
 	
 	int j = 0;
-	i = 0;
 	int qtdBitSig;
 	int qtdZeros;
 	unsigned int varUTF8;
-	//unsigned int bufferTemp[] = {0x0001D11E, 0x555, 0x0002D11E};
-	/*for (i = 1; i <3; i++)
-		bufferTemp[i] = inverte32(bufferTemp[i]);*/
 	unsigned char *p = &bufferWrite;
+		//printf("\n%p - %02X\n", p+3, *p+3);
+	wSize/=4;
+	i = 0;
 	while (wSize--)
-	{
+	{	
+		p = &bufferWrite[i-1];		//evita BOM
+		
+		if (bufferRead[i] <= 0x007F) 
+		{
+			
+				//printf("bufferRead[%d] eh: %02X\n", i, bufferRead[i]);
+				//printf("*p eh: %02X\n", *(p+3));
+			if ((*(p+3) & 0x40) == 0x40)
+			{
+				j=1;	// bit mais sig na 7a posicao
+			}
+			else if ((*(p+3) & 0x20) == 0x20)
+			{
+				j=2;	// bit mais sig na 6a posicao
+			}
+			else if ((*(p+3) & 0x10) == 0x10)
+			{
+				j=3;	// bit mais sig na 5a posicao
+			}
+			else if ((*(p+3) & 0x08) == 0x08)
+			{
+				j=4;	// bit mais sig na 4a posicao
+			}
+			else if ((*(p+3) & 0x04) == 0x04)
+			{
+				j=5;	// bit mais sig na 3a posicao
+			}
+			else if ((*(p+3) & 0x02) == 0x02)
+			{
+				j=6;	// bit mais sig na 2a posicao
+			}
+			else if ((*(p+3) & 0x01) == 0x01)
+			{
+				j = 7; 	// bit mais sig na 1a posicao, e é 1
+			}
+			else
+				j = 8;   // número é 0x00
+			
+		
+				//printf("saiu.\n");			
+				//printf("j saida = %d\n", j);
+			qtdBitSig = 8 - j;
+				//printf("qtdBitSig = %d\n", qtdBitSig);
+		
+			qtdZeros = 7 - qtdBitSig;
+				//printf("qtdZeros = %d\n", qtdZeros);			
+			switch (qtdZeros)
+			{
+			    case 7:
+			    varUTF8 = 0x00;
+			    break;
+			    
+			    case 6:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)(0)(0)(0)(0)(0)x
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x01);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 5:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)(0)(0)(0)(0)xx
+			    varUTF8 = bufferRead[i]&0x03; 		     	// anterior mais byte preenchido
+			    break;
+			    
+			    case 4:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)(0)(0)(0)xxx
+			    varUTF8 = bufferRead[i]&0x07;		      	// anterior mais byte preenchido
+			    break;
+			    
+			    case 3:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)(0)(0)xxxx
+			    varUTF8 = bufferRead[i]&0x0F;  		    	// anterior mais byte preenchido
+			    
+			    case 2:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)(0)xxxxx
+			    varUTF8 = bufferRead[i]&0x1F;  		    	// anterior mais byte preenchido
+			    break;
+			    
+			    case 1:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0(0)xxxxx
+			    varUTF8 = bufferRead[i]&0x3F;		      	// anterior mais byte preenchido
+			    break;
+			    
+			    case 0:
+			    varUTF8 = 0x00;					// molde"+0's na frente"   	          0xxxxxxx
+			    varUTF8 = bufferRead[i]&0x7F; 		     	// anterior mais byte preenchido
+			    break;
+			    
+			    default:
+			    	{fputs ("erro na leitura do valor UTF-32 na seção 1.\n", stderr); return -1;}
+			  }
+		
+			printf("O Valor UTF-32 0x%02X convertido para UTF-8 e: 0x%02X\n", bufferRead[i], varUTF8);
+			if (fwrite (&varUTF8,rSize-4,1,arq_saida))
+				printf("Caractere impresso no arquivo com sucesso!\n");
+			else
+				fputs ("erro de gravação do arquivo.", stderr);
+		}
+		
+		if (bufferRead[i] >= 0x0080 && bufferRead[i] <= 0x07FF) 
+		{
+			// 00000 00000 00000 00000 0000 0|000 1|000 0000        
+			// 00000 00000 00000 00000,0000 0|111 1|111 1111
+			
+				//printf("bufferRead[i] eh: %02X\n", bufferRead[i]);
+				//printf("*p eh: %02X\n", *p+2);
+			if ((*(p+2) & 0x04) == 0x04)
+			{
+				j=5;	// bit mais sig na 4a posicao
+			}
+			else if ((*(p+2) & 0x02) == 0x02)
+			{
+				j=6;	// bit mais sig na 3a posicao
+			}
+			else if ((*(p+2) & 0x01) == 0x01)
+			{
+				j=7;	// bit mais sig na 2a posicao
+			}
+			else
+				j=8;	// bit mais sig na 1a posicao
+			
+		
+				//printf("saiu.\n");			
+				//printf("j saida = %d\n", j);
+			qtdBitSig = 16 - j; // 8 a 11, j de 8 a 5
+				//printf("qtdBitSig = %d\n", qtdBitSig);
+		
+			qtdZeros = 11 - qtdBitSig;
+				//printf("qtdZeros = %d\n", qtdZeros);			
+			switch (qtdZeros)
+			{
+			    case 3:
+			    varUTF8 = 0xC080 & 0xE3FF;				// molde+0's na frente                 110(0)(0)(0)xx 10xxxxxx
+			    varUTF8 = varUTF8 | (bufferRead[i]>>6)<<8;    	// anterior mais 3o byte preenchido
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    
+			    break;
+			    
+			    case 2:
+			    varUTF8 = 0xC080 & 0xE7FF;		                // molde+0's na frente                 110(0)(0)xxx   10xxxxxx
+			    varUTF8 = varUTF8 | (bufferRead[i]>>6)<<8;    	// anterior mais 3o byte preenchido
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 1:
+			    varUTF8 = 0xC080 & 0xEFFF;		                // molde+0's na frente                   110(0)xxxx   10xxxxxx
+	   		    varUTF8 = varUTF8 | (bufferRead[i]>>6)<<8;    	// anterior mais 3o byte preenchido	  
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 0:
+			    varUTF8 = 0xC080 & 0xEFFF;		                // molde+0's na frente                    110xxxxx   10xxxxxx
+			    varUTF8 = varUTF8 | (bufferRead[i]>>6)<<8;    	// anterior mais 1o byte preenchido	  
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+
+			    default:
+			    {fputs ("erro na leitura do valor UTF-32 na seção 2.\n", stderr); return -1;}
+			  }
+		
+			printf("O Valor UTF-32 0x%04X convertido para UTF-8 e: 0x%04X\n", bufferRead[i], varUTF8);
+			if (fwrite (&varUTF8,rSize-4,1,arq_saida))
+				printf("Caractere impresso no arquivo com sucesso!\n");
+			else
+				fputs("erro de gravação do arquivo.", stderr);
+		}
+	
+		if (bufferRead[i] >= 0x0800 && bufferRead[i] <= 0xFFFF && bufferRead[i] != 0xFEFF) 
+		{
+			// sig 12 a 16 0000 0000 0000 0000 |0000 1|000 0000 0000
+			//             0000 0000 0000 0000 |1000 0|000 0000 0000
+						
+				//printf("bufferRead[i] eh: %02X\n", bufferRead[i]);
+				//printf("*p eh: %02X\n", *(p+2));
+			if (((*p+2) & 0x08) == 0x08)
+			{
+				j=6;	// bit mais sig na 5a posicao
+			}
+			else if (((*p+2) & 0x04) == 0x04)
+			{
+				j=7;	// bit mais sig na 4a posicao
+			}
+			else if (((*p+2) & 0x02) == 0x02)
+			{
+				j=8;	// bit mais sig na 3a posicao
+			}
+			else if (((*p+2) & 0x01) == 0x01)
+			{
+				j=9;	// bit mais sig na 2a posicao
+			}
+			else if (((*p+2) & 0x10) == 0x10)
+			{
+				j = 10; // bit mais sig na 1a posicao
+			}
+		
+				//printf("saiu.\n");			
+				//printf("j saida = %d\n", j);
+			qtdBitSig = 24 - j;
+				//printf("qtdBitSig = %d\n", qtdBitSig);
+		
+			qtdZeros = 16 - qtdBitSig;
+				//printf("qtdZeros = %d\n", qtdZeros);			
+			switch (qtdZeros)
+			{
+			    case 4:
+			    varUTF8 = 0xF08080;
+			    varUTF8 = varUTF8 | (bufferRead[i]>>6)<<8;    	// anterior mais 3o byte preenchido
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    
+			    break;
+			    
+			    case 3:
+			    varUTF8 = 0xF08080;
+			    varUTF8 = varUTF8 | (bufferRead[i]>>12)<<16;    	// anterior mais 2o byte preenchido
+			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;   // anterior mais 3o byte preenchido
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 2:
+		            varUTF8 = 0x00F08080;
+	   		    varUTF8 = varUTF8 | (bufferRead[i]>>12)<<16;    	// anterior mais 2o byte preenchido	  
+			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;    // anterior mais 3o byte preenchido	  
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 1:
+			    varUTF8 = 0xF08080; //& 0xF0FFFF			
+			    varUTF8 = varUTF8 | ((bufferRead[i]>>12)&0x07)<<16;    	// anterior mais 2o byte preenchido	  
+			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;    // anterior mais 3o byte preenchido	  
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    case 0:
+                            varUTF8 = 0x00F08080;
+			    varUTF8 = varUTF8 | (bufferRead[i]>>12)<<16;    	// anterior mais 2o byte preenchido	  
+			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;    // anterior mais 3o byte preenchido	  
+
+			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
+			    break;
+			    
+			    default:
+			    {fputs ("erro na leitura do valor UTF-32 na seção 3.\n", stderr); return -1;}
+			  }
+		
+			printf("O Valor UTF-32 0x%06X convertido para UTF-8 e: 0x%06X\n", bufferRead[i], varUTF8);
+			if (fwrite (&varUTF8,rSize-4,1,arq_saida))
+				printf("Caractere impresso no arquivo com sucesso!\n");
+			else
+				fputs("erro de gravação do arquivo.", stderr);
+		}
 
 		if (bufferRead[i] >= 0x10000 && bufferRead[i] <= 0x10FFFF) 
 		{
 			
-			printf("bufferRead[i] eh: %02X\n", bufferRead[i]);
-			printf("*p eh: %02X\n", *p+1);
+				//printf("bufferRead[%d] eh: %02X\n", i, bufferRead[i]);
+				//printf("*p eh: %02X\n", *p+1);
 			if (((*p+1) & 0x10) == 0x10)
 			{
 				j=3;	// bit mais sig na 5a posicao
@@ -257,16 +504,16 @@ int utf32_8(FILE* arq_entrada, FILE* arq_saida)
 			
 		
 				//printf("saiu.\n");			
-				printf("j saida = %d\n", j);
+				//printf("j saida = %d\n", j);
 			qtdBitSig = 24 - j;
-				printf("qtdBitSig = %d\n", qtdBitSig);
+				//printf("qtdBitSig = %d\n", qtdBitSig);
 		
 			qtdZeros = 21 - qtdBitSig;
-				printf("qtdZeros = %d\n", qtdZeros);			
+				//printf("qtdZeros = %d\n", qtdZeros);			
 			switch (qtdZeros)
 			{
 			    case 4:
-			    varUTF8  = 0xF0808080 & 0xF8DFFFFF;			// molde+0's na frente 11110(000) 10(0)xxxxx 10xxxxxx 10xxxxxx
+			    varUTF8  = 0xF0808080 & 0xF8DFFFFF;			// molde+0s na frente 11110(000) 10(0)xxxxx 10xxxxxx 10xxxxxx
 			    varUTF8 = varUTF8 | (bufferRead[i]>>12)<<16;    	// anterior mais 2o byte preenchido
 			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;      // anterior mais 3o byte preenchido
 			    varUTF8 = varUTF8 | (bufferRead[i]&0x3F);      	// anterior mais 4o byte preenchido
@@ -297,7 +544,7 @@ int utf32_8(FILE* arq_entrada, FILE* arq_saida)
 			    break;
 			    
 			    case 0:
-			    varUTF8  = 0xF0808080;		//		Só molda sem inserir 0's   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			    varUTF8  = 0xF0808080;		//		Só molda sem inserir 0s   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
 			    varUTF8 = varUTF8 | (bufferRead[i]>>18)<<24;    	// anterior mais 1o byte preenchido	  
 			    varUTF8 = varUTF8 | ((bufferRead[i]>>12)&0x3F)<<16;    // anterior mais 2o byte preenchido	  
 			    varUTF8 = varUTF8 | ((bufferRead[i]>>6)&0x3F)<<8;      // anterior mais 3o byte preenchido
@@ -305,18 +552,23 @@ int utf32_8(FILE* arq_entrada, FILE* arq_saida)
 			    break;
 			    
 			    default:
-			    {fputs ("erro na leitura do valor UTF-32.", stderr); return -1;}
+			    {fputs ("erro na leitura do valor UTF-32 na seção 4.\n", stderr); return -1;}
 			  }
 		
-			printf("Valor original convertido para UTF-8 e: %08X\n", varUTF8);
+			printf("O Valor UTF-32 0x%06X convertido para UTF-8 e: 0x%06X\n", bufferRead[i], varUTF8);
+			if (fwrite (&varUTF8,rSize-4,1,arq_saida))
+				printf("Caractere impresso no arquivo com sucesso!\n");
+			else
+				fputs("erro de gravação do arquivo.", stderr);
+				
 		}		
 		
 	
 			//printf("conteudo de bufferWrite como char: %02X\n", *p);
 		i++;
-		p+=4;	
+		//p+=4;	// end while
 	}
-			
+	printf("\nrepetições: %d\n", i);		
 	free(bufferRead);
 	free(bufferWrite);
 	return 0;
